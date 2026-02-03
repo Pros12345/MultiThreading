ef7f931547ce2c0c3df46af230c5e760ce48020e
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-04 00:51:43 +0530

    Add files via upload
    changes on priority thread

--- PriorityThread.java
+++ PriorityThread.java
@@ -1,16 +1,31 @@
 
 public class PriorityThread extends Thread {
 
+	public PriorityThread(String name) {
+		super(name);
+	}
+
 	public static void main(String[] args) {
 
-		PriorityThread pt = new PriorityThread();
-		pt.start();
+		PriorityThread t1 = new PriorityThread("MIN_PRIORITY");
+		PriorityThread t2 = new PriorityThread("NORM_PRIORITY");
+		PriorityThread t3 = new PriorityThread("MAX_PRIORITY");
+		t1.setPriority(MIN_PRIORITY);
+		t2.setPriority(NORM_PRIORITY);
+		t3.setPriority(MAX_PRIORITY);
+		t1.start();
+		t2.start();
+		t3.start();
 	}
 
 	@Override
 	public void run() {
 
 		for (int i = 0; i < 5; i++) {
+			String s = "";
+			for (int j = 0; j < 10000; j++) {
+				s += "s";
+			}
 			System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority()
 					+ " - Count: " + i);
 
