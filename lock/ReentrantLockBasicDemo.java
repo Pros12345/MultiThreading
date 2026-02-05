54e3245a77763f7561dd55f60c8d0c52e9f51bd1
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-05 21:23:06 +0530

    Add files via upload

--- lock/ReentrantLockBasicDemo.java
+++ lock/ReentrantLockBasicDemo.java
@@ -1,1 +1,36 @@
+package lock;
 
+import java.util.concurrent.locks.ReentrantLock;
+
+class Counter {
+	private int count = 0;
+	private ReentrantLock lock = new ReentrantLock();
+
+	public void increment() {
+		lock.lock(); // acquire lock
+		try {
+			count++;
+			System.out.println(Thread.currentThread().getName() + " -> " + count);
+		} finally {
+			lock.unlock(); // always release lock
+		}
+	}
+}
+
+public class ReentrantLockBasicDemo {
+	public static void main(String[] args) {
+		Counter counter = new Counter();
+
+		Runnable task = () -> {
+			for (int i = 0; i < 5; i++) {
+				counter.increment();
+			}
+		};
+
+		Thread t1 = new Thread(task, "Thread-1");
+		Thread t2 = new Thread(task, "Thread-2");
+
+		t1.start();
+		t2.start();
+	}
+}
