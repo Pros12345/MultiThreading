3770a5e93bf41239241c34c98f733ab108768f82
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-17 22:51:40 +0530

    Add files via upload

---
+++ FutureVariable.java
@@ -0,0 +1,21 @@
+
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureVariable {
+
+    public static void main(String[] args)
+            throws InterruptedException, ExecutionException {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Future<Integer> future = executorService.submit(() -> 42);
+
+        System.out.println(future.get());
+
+        executorService.shutdown();
+    }
+}
